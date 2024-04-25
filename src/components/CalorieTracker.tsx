19ace12e32ff1ed4762a1f0c08f3f2b761a9423a
Jose David <lujan9495@gmail.com>
2024-04-25 08:50:54 -0500

    Creando un componente para las cantidades

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { Activity } from "../types/types"
+import CalorieDisplay from "./CalorieDisplay"
 
 type CalorieTrackerProps = {
     activities: Activity[]
@@ -17,15 +18,15 @@ export default function CalorieTracker({ activities } : CalorieTrackerProps) {
             <h2 className=" text-4xl font-black text-white text-center">Resumen de Calorías</h2>
 
             <div className=" flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-                <p className=" text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className=" font-black text-6xl text-orange">{caloriesConsumed}</span>
-                    Consumidas
-                </p>
+                <CalorieDisplay 
+                    calories={caloriesConsumed}
+                    text="Consumidas"
+                />
                 
-                <p className=" text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className=" font-black text-6xl text-orange">{caloriesBurned}</span>
-                    Ejercicio
-                </p>
+                <CalorieDisplay 
+                    calories={caloriesBurned}
+                    text="Ejercicio"
+                />
             </div>
 
         </>
