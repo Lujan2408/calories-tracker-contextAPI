ff57658eddbde508f9d45c3be142f20872ac741d
Jose David <lujan9495@gmail.com>
2024-04-26 18:33:28 -0500

    Validar para cambiar el color del texto según las calorias quemadas o consumidas

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <header className=' bg-lime-600 py-3'>
+      <header className=' bg-lime-700 py-3'>
         <div className=' max-w-4xl mx-auto flex justify-between'>
           <h1 className=' text-center text-lg font-bold text-white uppercase'>
             Contador Calorias
--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -12,7 +12,18 @@ export default function CalorieTracker({ activities } : CalorieTrackerProps) {
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
     
     const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
-  
+
+    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned,[caloriesBurned, caloriesConsumed])
+
+    //Validar para cambiar el color del texto
+    let textColor = 'text-white'
+        
+    if(netCalories > 0) {
+        textColor = 'text-green-400'
+    } else if(netCalories < 0) {
+        textColor = 'text-red-500'
+    }
+
     return (
         <>
             <h2 className=" text-4xl font-black text-white text-center">Resumen de Calorías</h2>
@@ -27,6 +38,12 @@ export default function CalorieTracker({ activities } : CalorieTrackerProps) {
                     calories={caloriesBurned}
                     text="Ejercicio"
                 />
+                
+                <p className=" text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                    <span className={` font-black text-6xl text-orange ${textColor}`}>{netCalories}</span>
+                    Diferencia
+                </p>
+                
             </div>
 
         </>
